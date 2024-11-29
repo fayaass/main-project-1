0e6c63c84b58121dd050a4c662baefc2a2cf12c6
fayaass <muhammedfayas59504@gmail.com>
2024-11-29 12:56:34 +0530

    add

---
+++ watch/app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Product(models.Model):
+    pro_id=models.TextField()
+    name=models.TextField()
+    price=models.IntegerField()
+    ofr_price=models.IntegerField()
+    img=models.FileField()
+    dis=models.TextField()
+
+# class Cart(models.Model):
+#     user=models.ForeignKey(User,on_delete=models.CASCADE)
+#     product=models.ForeignKey(Product,on_delete=models.CASCADE)
+
+# class Buy(models.Model):
+#     user=models.ForeignKey(User,on_delete=models.CASCADE)
+#     product=models.ForeignKey(Product,on_delete=models.CASCADE)
+#     price=models.IntegerField()
+#     data=models.DateField(auto_now_add=True)
+# Create your models here.
---
+++ watch/app/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.shp_login),
+    path('shp_logout',views.shp_logout),
+
+
+    # ---------------shop--------------------
+
+
+    path('shp_home',views.shp_home),
+    path('add_prod',views.add_prod),
+    path('edit_prod/<pid>',views.edit_prod),
+    path('delete_prod/<pid>',views.delete_prod),
+
+
+    # --------------user---------------------
+
+
+    path('register',views.register),
+    path('user_home',views.user_home),
+    path('view_pro/<pid>',views.view_pro),
+    
+
+]
